a3b1a414dfcc3a05c501d8a2403d54081dfd485f
Konstantin Gerstendörfer <konstantin@gerstendoerfer.de>
2022-10-17 22:14:53 +0200

    Fügt Steuerung per HTTP Requests hinzu

--- README.md
+++ README.md
@@ -2,25 +2,33 @@
 
 ## Funktion
 
-Schaltet per Taster den Monitor in der Fahrzeughalle an oder aus.  
-Um den Monitor an-/auszuschalten zieht man, bei Verwendung eines Pi Zeros, mit dem Taster `GPIO 18` auf `GND`. Wenn ein anderes Board verwendet wird, muss evtl. der GPIO Pin im Skript angepasst werden.
+Schaltet per Taster oder HTTP Request den Monitor in der Fahrzeughalle an oder aus.
 
-## Einrichtung: Skript als Service in Raspbian ausführen
+## Einrichtung: Skript als Service in Raspberry Pi OS ausführen
 
 ### Voraussetzungen
 
 - Der Fernseher muss immer vor der Stromversorgung an den Raspberry Pi angeschlossen werden.
 - Alle HDMI-CEC-Steueroptionen müssen am Fernseher aktiviert sein.
-- In den Sperr-Optionen des Fernsehers sollten über den Hotel-Modus alle Eingangsquellen, außer der Monitor, gesperrt werden (Standard Passwort zum entsperren: `0000`).  
+- In den Sperr-Optionen des Fernsehers sollten über den Hotel-Modus alle Eingangsquellen, außer der FF-Agent Monitor, gesperrt werden (Standard Passwort zum entsperren: `0000`).  
   (Das ist wichtig, um zu gewährleisten, dass nach einem Neustart aller Systeme die richtige Quelle im Fernseher gewählt ist.)
+
+### Funktionierende Umgebung
+
+- Raspberry Pi OS 10 (buster) auf einem Pi Zero W
+- Python 3.7
+- python-cec 0.2.8
 
 ### Installation
 
 1. **Python Modul [`python-cec`](https://github.com/trainman419/python-cec) mit Abhängigkeiten installieren:**
 
-       sudo apt install libcec-dev build-essential python-dev && pip install cec
+       sudo apt install libcec-dev build-essential python3-dev && pip3 install cec
 
-2. **`monitor-control.py` nach `/usr/bin` kopieren.**
+2. **Repository nach `/usr/bin` klonen:**
+
+       sudo git clone https://github.com/gersko/fw-monitor-control /usr/bin/fw-monitor-control
+
 3. **Service erstellen:**
 
        sudo nano /lib/systemd/system/monitor-control.service
@@ -32,7 +40,7 @@ Um den Monitor an-/auszuschalten zieht man, bei Verwendung eines Pi Zeros, mit d
 
        [Service]
        Type=simple
-       ExecStart=/usr/bin/python /usr/bin/monitor-control.py
+       ExecStart=/usr/bin/python3 /usr/bin/fw-monitor-control/monitor-control.py
        User=pi
 
        [Install]
@@ -54,6 +62,24 @@ Um den Monitor an-/auszuschalten zieht man, bei Verwendung eines Pi Zeros, mit d
    Dazu folgende Zeile in `/boot/config.txt` ***ent***kommentieren:
 
        hdmi_force_hotplug=1
+
+## Bedienung
+
+### Monitor per Taster steuern
+
+Um den Monitor an-/auszuschalten zieht man, bei Verwendung eines Pi Zeros, mit dem Taster `GPIO 18` auf `GND`. Wenn ein anderes Board verwendet wird, muss evtl. der GPIO Pin im Skript angepasst werden.
+
+### Monitor per HTTP Request steuern
+
+Der Monitor ebenso über folgende HTTP Requests gesteuert werden:
+
+#### Monitor anschalten:
+
+       GET http://<ip-address>:8080/monitor?turn=on
+
+#### Monitor ausschalten:
+
+       GET http://<ip-address>:8080/monitor?turn=off
 
 ## Troubleshooting Hilfe: Der `cec-client`
 
--- monitor-control.py
+++ monitor-control.py
@@ -2,8 +2,11 @@
 
 from sys import stdout
 import RPi.GPIO as GPIO
-from time import sleep, time
+import time
 import cec
+import http.server
+import socketserver
+import threading
 
 
 #################
@@ -16,8 +19,48 @@ GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 def print_(content):
     print(content)
-    stdout.flush()  # Flush output buffer after each
-                    # print for proper logging on Linux
+    stdout.flush()  # Flush output buffer after each print for proper logging on Linux
+
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        global http_action
+        if self.path == "/monitor?turn=on":
+            print_("HTTP request: turn monitor on.")
+            http_action = "turn_on"
+            self.send_response(200)
+        elif self.path == "/monitor?turn=off":
+            print_("HTTP request: turn monitor off.")
+            http_action = "turn_off"
+            self.send_response(200)
+        else:
+            self.send_response(400)
+
+def server_thread_target():
+    while True:
+        try:
+            with socketserver.TCPServer(("", 8080), RequestHandler) as httpd:
+                print_("HTTP server started.")
+                httpd.serve_forever()
+        except Exception as e:
+            print_("HTTP server failed: " + str(e))
+            print_("Restarting HTTP server in 10s...")
+            time.sleep(10)
+
+def toggle_monitor(action=None):
+    if action is None:
+        if cec_tv.is_on():
+            action = "turn_off"
+        else:
+            action = "turn_on"
+    try:
+        if action == "turn_off":
+            print_("Turning monitor off...")
+            cec_tv.standby()
+        elif action == "turn_on":
+            print_("Turning monitor on...")
+            cec_tv.power_on()
+    except Exception as e:
+        print_("Toggling monitor failed: " + str(e))
 
 
 ##########
@@ -36,41 +79,50 @@ except Exception as e:
 
 cec_tv = cec.Device(cec.CECDEVICE_TV)
 
-currentTime = 0
-referenceTime = 0
-elapsedTime = 0
+http_action = None  # Pending action via http (always reset to None if no action is pending)
 
-try:
+server_thread = threading.Thread(target=server_thread_target, daemon=True)
+server_thread.start()
 
-    while True:
+time_current = 0
 
-        currentTime = time()
+time_button_last_off = time_current
+time_button_last_action = 0
 
-        if GPIO.input(18) != 0:
+time_http_last_action = 0
 
-            elapsedTime += currentTime - referenceTime
+while True:
 
-            if elapsedTime > .05:
+    time_current = time.time()
 
-                try:
+    # Toggle monitor via push-button
+    if GPIO.input(18) == 0:
+        time_button_last_off = time_current
+    else:
+        if (
+            time_current - time_button_last_off >= .05 and
+            time_current - time_button_last_action >= 1
+        ):
+            time_button_last_action = time_current
+            print_("Push-button: toggle monitor.")
+            toggle_monitor()
+
+    # Toggle monitor via HTTP request
+    if not http_action is None:
+        if time_current - time_http_last_action >= 1:
+            time_http_last_action = time_current
+            try:
+                if http_action == "turn_on":
                     if cec_tv.is_on():
-                        cec_tv.standby()
-                        print_("Turned monitor off.")
+                        http_action = None
                     else:
-                        cec_tv.power_on()
-                        print_("Turned monitor on.")
-                except Exception as e:
-                    print_("Toggling power failed: " + str(e))
+                        toggle_monitor("turn_on")
+                elif http_action == "turn_off":
+                    if not cec_tv.is_on():
+                        http_action = None
+                    else:
+                        toggle_monitor("turn_off")
+            except OSError:
+                pass
 
-                sleep(.5)
-
-        else:
-
-            elapsedTime = 0
-
-        referenceTime = currentTime
-
-except KeyboardInterrupt:
-
-    GPIO.cleanup()
-    print_("Monitor control exiting...")
+    time.sleep(.01)
