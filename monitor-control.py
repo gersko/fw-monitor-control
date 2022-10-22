f64c0282fa5801c2083f6188ec4461ac045ef336
Konstantin Gerstendörfer <konstantin@gerstendoerfer.de>
2022-10-22 19:09:25 +0200

    Räumt auf und verbessert Exception Handling

--- monitor-control.py
+++ monitor-control.py
@@ -13,16 +13,80 @@ import threading
 #  Definitions  #
 #################
 
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-
 def print_(content):
     print(content)
-    stdout.flush()  # Flush output buffer after each print for proper logging on Linux
+    stdout.flush()  # Flush output buffer after each print
+                    # for proper logging on Linux
+
+def cec_init():
+    global cec_reinit
+
+    print_("Initializing CEC...")
+    success = False
+    try:
+        cec.init()
+        print_("CEC initialization successful.")
+        cec_reinit = False
+        success = True
+    except Exception as e:
+        print_(f"CEC initialization failed: {e}")
+
+    return success
+
+def get_monitor_state():
+    global cec_reinit
+
+    print_("Retrieving monitor state...")
+    state = None
+    try:
+        if cec_tv.is_on():
+            state = "on"
+        else:
+            state = "off"
+        print_(f"Monitor is turned {state}.")
+    except OSError as e:
+        print_(
+            f"Failed to retrieve monitor state: {e}. "
+            f"Reinitializing CEC.")
+        cec_reinit = True
+
+    return state
+
+def toggle_monitor(action=None):
+    global cec_reinit
+    global http_action
+
+    print_(f"Toggle monitor (action={action})...")
+
+    cec_tv_state = get_monitor_state()
+    if cec_tv_state is None:
+        return
+    elif (
+        (cec_tv_state == "on" and action == "turn_on") or
+        (cec_tv_state == "off" and action == "turn_off")
+    ):
+        http_action = None
+        return
+    elif cec_tv_state == "on" and action is None:
+        action = "turn_off"
+    elif cec_tv_state == "off" and action is None:
+        action = "turn_on"
+
+    try:
+        if action == "turn_off":
+            print_("Turning monitor off...")
+            cec_tv.standby()
+            print_("Successfully sent standby command.")
+        elif action == "turn_on":
+            print_("Turning monitor on...")
+            cec_tv.power_on()
+            print_("Successfully sent power on command.")
+    except Exception as e:
+        print_(f"Toggling monitor failed: {e}.")
 
 class RequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
+        global cec_reinit
         global http_action
         if self.path == "/monitor?turn=on":
             print_("HTTP request: turn monitor on.")
@@ -38,79 +102,72 @@ class RequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Turning monitor off...")
-        elif self.path == "/monitor?status":
-            print_("HTTP request: send status.")
-            try:
-                if cec_tv.is_on():
-                    status = b"Monitor is on."
-                else:
-                    status = b"Monitor is off."
-            except Exception as e:
-                status = str.encode("Failed to get the status: " + str(e))
+        elif self.path == "/monitor?state":
+            print_("HTTP request: retrieve monitor state.")
+            cec_tv_state = get_monitor_state()
+            if cec_tv_state is None:
+                state = (
+                    f"Failed to retrieve monitor state. "
+                    f"Reinitializing CEC, try again.")
+            else:
+                state = f"Monitor is turned {cec_tv_state}."
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(status)
+            self.wfile.write(str.encode(state))
         else:
             self.send_response(400)
 
 def server_thread_target():
     while True:
+        print_("Starting HTTP server...")
         try:
             with socketserver.TCPServer(("", 8080), RequestHandler) as httpd:
                 print_("HTTP server started.")
                 httpd.serve_forever()
-        except Exception as e:
+        except OSError as e:
             print_("HTTP server failed: " + str(e))
-            print_("Restarting HTTP server in 10s...")
-            time.sleep(10)
-
-def toggle_monitor(action=None):
-    if action is None:
-        if cec_tv.is_on():
-            action = "turn_off"
-        else:
-            action = "turn_on"
-    try:
-        if action == "turn_off":
-            print_("Turning monitor off...")
-            cec_tv.standby()
-        elif action == "turn_on":
-            print_("Turning monitor on...")
-            cec_tv.power_on()
-    except Exception as e:
-        print_("Toggling monitor failed: " + str(e))
+            print_("Restarting HTTP server in 30s...")
+            time.sleep(30)
 
 
 ##########
 #  Main  #
 ##########
 
-print_("Monitor control started.")
+cec_reinit = False  # Specifies whether CEC needs to be reinitialized
+http_action = None  # Pending action of the latest HTTP request
+                    # (always reset to None if no action is pending)
 
-try:
-    cec.init()
-    print_("Init successful.")
-except Exception as e:
-    print_("Init failed: " + str(e))
-    print_("Monitor control exiting...")
-    exit()
-
-cec_tv = cec.Device(cec.CECDEVICE_TV)
-
-http_action = None  # Pending action via http (always reset to None if no action is pending)
-
-server_thread = threading.Thread(target=server_thread_target, daemon=True)
-server_thread.start()
-
-time_current = 0
-
-time_button_last_off = time_current
+time_button_last_off = 0
 time_button_last_action = 0
 
 time_http_last_action = 0
 
+print_("Monitor control started.")
+
+# Initialize CEC
+if not cec_init():
+    print_("Monitor control exiting...")
+    exit()
+cec_tv = cec.Device(cec.CECDEVICE_TV)
+get_monitor_state()
+
+# Initialize GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# Start HTTP server
+server_thread = threading.Thread(target=server_thread_target, daemon=True)
+server_thread.start()
+
 while True:
+
+    # Reinitialize CEC if necessary
+    if cec_reinit:
+        if not cec_init():
+            time.sleep(10)
+            continue
 
     time_current = time.time()
 
@@ -128,20 +185,8 @@ while True:
 
     # Toggle monitor via HTTP request
     if not http_action is None:
-        if time_current - time_http_last_action >= 1:
+        if time_current - time_http_last_action >= 5:
             time_http_last_action = time_current
-            try:
-                if http_action == "turn_on":
-                    if cec_tv.is_on():
-                        http_action = None
-                    else:
-                        toggle_monitor("turn_on")
-                elif http_action == "turn_off":
-                    if not cec_tv.is_on():
-                        http_action = None
-                    else:
-                        toggle_monitor("turn_off")
-            except OSError:
-                pass
+            toggle_monitor(http_action)
 
     time.sleep(.01)
